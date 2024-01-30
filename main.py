c7332dfee74263e082129f79dddf6a9810108765
nefalis <nefalis@gmail.com>
2024-01-30 15:45:58 +0100

    recupération de one_category

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 import urllib.request
-from fonctions.one_category import one_category
-from fonctions.one_book import recup_book
+from fonctions.one_category2 import one_category
+# from fonctions.one_book import recup_book
 
-url_one_book = "http://books.toscrape.com/catalogue/charlie-and-the-chocolate-factory-charlie-bucket-1_13/index.html"
+"""url_one_book = "http://books.toscrape.com/catalogue/charlie-and-the-chocolate-factory-charlie-bucket-1_13/index.html"
 print(recup_book(url_one_book))
+"""
 
+print("debut scrapping")
 url_one_category = "https://books.toscrape.com/catalogue/category/books_1/"
-
+# Définir un nom de fichier spécifique
+file_name = "exemple.csv"
 print(one_category(url_one_category, file_name))
+print("fin scrapping")
