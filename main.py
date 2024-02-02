79ae91f12f2a2ecbbf3ddf0a1ef731fa13017ee9
nefalis <nefalis@gmail.com>
2024-02-02 11:23:21 +0100

    update suite a git pull

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
-import urllib.request
+
 from fonctions.one_category2 import one_category
-# from fonctions.one_book import recup_book
 
-"""url_one_book = "http://books.toscrape.com/catalogue/charlie-and-the-chocolate-factory-charlie-bucket-1_13/index.html"
-print(recup_book(url_one_book))
-"""
+print("debut")
+# Récupération de la page d'acceuil. page number permet d'initialisé la page actuelle a 1.
+base_url = "http://books.toscrape.com/"
+page_number = 1
 
-print("debut scrapping")
-url_one_category = "https://books.toscrape.com/catalogue/category/books_1/"
-# Définir un nom de fichier spécifique
-file_name = "exemple.csv"
-print(one_category(url_one_category, file_name))
-print("fin scrapping")
+# obtenir le contenu HTML de la page actuelle et beautiful pour analyser l'HTML
+response = requests.get(base_url)
+soup = BeautifulSoup(response.content, 'lxml')
+link_category = []
+
+# Trouver le menu de navigation contenant les catégories
+nav_menu = soup.find('div', class_='side_categories')
+# Trouver toutes les catégories
+categories = nav_menu.find_all('a')
+
+# Afficher les catégories
+#  la méthode .strip() est utilisée pour supprimer les espaces et les sauts de ligne éventuels autour du texte
+for category in categories:
+    print("je cherche des cat")
+    category_name = category.text.strip()
+    category_url = base_url + category['href']
+    category_url = category_url.replace('index.html', '')
+    link_category.append(category_url)
+    print("liste cat")
+    print("url cat", category_url)
+    print("nom cat", category_name)
+
+    name_csv = "{category_name}.csv"
+    print("avant appel one cat")
+    one_category(category_url, name_csv)
+print("fin")
