d99b4dd6031b764b229c74fe881ad485053c4cee
nefalis <nefalis@gmail.com>
2024-02-08 09:14:42 +0100

    résolution du probleme de categorie

--- fonctions/one_category.py
+++ fonctions/one_category.py
@@ -1,77 +1,55 @@
 # librairies utilisées
 import requests
 from bs4 import BeautifulSoup
-import urllib.request
 import os
 from fonctions.save_picture import recup_img
 from fonctions.create_csv import write_csv
+from fonctions.save_picture import save_img
 
-# fonction regroupant toute les fonctions d'extraction de donnée des livres d'une catégorie
-# qui sera ensuite appelé dans le main
-def one_category(url_one_category, file_name):
+# fonction pour extraire les informations des livres d'une catégorie
+def one_category(base_url, file_name):
+    print("start one cat")
+    # Nom du dossier où seront stocké les données extraites
     data_folder = 'data_file'
+    # Chemin du dossier image
 
-    # Vérification si les dossier existe
+    # Vérification (exists) et création des dossiers (makedirs)
     if not os.path.exists(data_folder):
         os.makedirs(data_folder)
-
-    # Supprimer le fichier CSV s'il existe déjà
     if os.path.exists(file_name):
         os.remove(file_name)
 
+    # Page number permet d'initialisé la page actuelle a 1.
     page_number = 1
+    next_page_url = base_url
 
-    # Fonction pour récuperer l'url de la categorie. page number permet d'initialisé la page actuelle a 1.
-    def url_category(url_one_category):
-        # pour récupere l'url de la categorie
-        response = requests.get(url_one_category)
-        soup = BeautifulSoup(response.content, 'lxml')
-        return soup
-
-    # fonction pour recupérer les url des livres d'une category
-    def url_book_category(next_page_url):
-        # obtenir le contenu HTML de la page actuelle et beautiful pour analyser l'HTML
+    # Boucle pour parcourir les pages de la catégorie
+    while next_page_url:
+        print("boucle while")
+        # Récupération du contenu HTML de la page actuelle et beautiful pour analyser l'HTML
         response = requests.get(next_page_url)
         soup = BeautifulSoup(response.content, 'lxml')
 
         # Récupération des liens des livres sur la page actuelle
         book_links = soup.find_all('h3')
+
+        # Boucle sur les liens des livres pour extraire les informations
         for book_link in book_links:
-            # boucle sur les liens pour construire url final
+            print("je recup un livre")
             relative_url = book_link.a['href']
-            final_url = url_one_category + relative_url
-            return final_url
+            # URL complet du livre
+            final_url = base_url + relative_url
 
-    # fonction pour extraire les information des livres
-    def info_book_category(final_url, next_page_url, file_name):
-        # Écrire l'en-tête une seule fois avant la boucle
-        header = ["product_page_url",
-                  "universal_ product_code",
-                  "title",
-                  "price_including_tax",
-                  "price_excluding_tax",
-                  "number_available",
-                  "product_description",
-                  "category",
-                  "review_rating",
-                  "image_url"]
-        write_csv(file_name, header)
-
-        # boucle pour parcourir les pages
-        while next_page_url:
-            # extraction des donnes sur la page de chaque livre
-            # 2eme requete pour acceder à la page specifique  du livre
+            # extraction des données sur la page de chaque livre
             response_book = requests.get(final_url)
-            # utilisation de beautiful soup pour analyse de la page du livre
             soup_book = BeautifulSoup(response_book.content, 'lxml')
 
+            # Extraction des informations spécifiques du livre
             product_page_url = final_url
             title = soup_book.find("h1").text
-            print("titre", title)
             review_rating = soup_book.find('p', class_='star-rating').get('class').pop()
             product_description = soup_book.find("article", {"class": "product_page"}).find_all("p")[3].text
             category = soup_book.find("ul", {"class": "breadcrumb"}).find_all("a")[2].text
-            print("categorie", category)
             # On recherche tout les elements td de la page
             list_table = soup_book.find_all('td')
             # On recherche de l'element précis contenu uniquement dans les td
@@ -80,7 +58,7 @@ def one_category(url_one_category, file_name):
             price_excluding_tax = list_table[3].text
             number_available = list_table[5].text
 
-            # pour enregistrer l'image
+            # Récupération de l'URL de l'image du livre
             image_url = recup_img(soup_book)
 
             # Ajouter les données du livre à la liste
@@ -95,38 +73,25 @@ def one_category(url_one_category, file_name):
                     review_rating,
                     image_url]
 
-            # Appeler la fonction pour écrire dans le fichier CSV
+            # Appel de la fonction pour écrire dans le fichier CSV
             file_name = category
             write_csv(file_name, data, category)
 
-            # enlever les caractere non supporter pour le nom de l'image
-            clean_title = title.replace(":", "").replace("/", " ").replace('"', '').replace(
-                'Ã©', 'é').replace(",", "").replace(".", "").replace("&", "").replace("*", "").replace("?", "").replace(
-                "#", "")
-            name_img = f"{clean_title}_image.jpg"
-            image_path = os.path.join('data_file', category, name_img)
-            # urlretrieve va enregistré l'image avec son nom et l'image
-            urllib.request.urlretrieve(image_url, image_path)
+            save_img(image_url, title, category)
 
-            # Chercher le lien vers la page suivante Mise à jour de next_page_url si une page suivante existe, sinon,
-            # le définir sur None pour arrêter la boucle.
-            next_page = soup.find('li', class_='next')
-            if next_page:
-                next_page_url = url_one_category + next_page.a['href']
-                page_number += 1
-            else:
-                next_page_url = None
+            print("category livre", category)
 
-    soup = url_category(url_one_category)
-    next_page_url = url_one_category  # Initialisation de next_page_url avec l'URL de la catégorie
-    final_url = url_book_category(next_page_url)
-    info_book_category(final_url, next_page_url, file_name)
+        # Mise à jour de next_page_url si une page suivante existe, sinon, le définir sur None pour arrêter la boucle.
+        next_page = soup.find('li', class_='next')
+        if next_page:
+            next_page_url = base_url + next_page.a['href']
+            page_number += 1
+        else:
+            next_page_url = None
 
+"""# Définition des paramètres de la fonction One_category
+base_url = "https://books.toscrape.com/catalogue/category/books/childrens_11/"
+file_name = "exemple.csv"
 
-url_one_category = "https://books.toscrape.com/catalogue/category/books/childrens_11/"
-file_name = category + '.csv'
-one_category(url_one_category, file_name)
-
-# Fin
-print(f"Extraction terminée.")
-print("fichier csv fait")
+# Appel de la fonction pour extraire les données
+one_category(base_url, file_name)"""
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-from fonctions.one_category2 import one_category
+from fonctions.one_category import one_category
 
 print("debut")
 # Récupération de la page d'acceuil. page number permet d'initialisé la page actuelle a 1.
-base_url = "http://books.toscrape.com/"
+base_url = "https://books.toscrape.com/"
 page_number = 1
 
 # obtenir le contenu HTML de la page actuelle et beautiful pour analyser l'HTML
@@ -14,23 +14,20 @@ soup = BeautifulSoup(response.content, 'lxml')
 link_category = []
 
 # Trouver le menu de navigation contenant les catégories
-nav_menu = soup.find('div', class_='side_categories')
+nav_menu = soup.select('.nav-list >li > ul')
 # Trouver toutes les catégories
-categories = nav_menu.find_all('a')
+categories = nav_menu[0].find_all('a')
 
 # Afficher les catégories
 #  la méthode .strip() est utilisée pour supprimer les espaces et les sauts de ligne éventuels autour du texte
 for category in categories:
-    print("je cherche des cat")
     category_name = category.text.strip()
     category_url = base_url + category['href']
     category_url = category_url.replace('index.html', '')
-    link_category.append(category_url)
-    print("liste cat")
-    print("url cat", category_url)
-    print("nom cat", category_name)
 
     name_csv = "{category_name}.csv"
-    print("avant appel one cat")
     one_category(category_url, name_csv)
+
 print("fin")
+print("fichier data fait !! ")
+
